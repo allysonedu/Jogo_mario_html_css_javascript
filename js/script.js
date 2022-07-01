0f83e1e8a86e321d340e61545ea3783c798548fd
allyson <allysoneduu157@gmail.com>
2022-07-01 15:23:27 -0300

    Mario jump

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const Mario = document.querySelector('.Mario');
+
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+  Mario.classList.add('jump');
+
+  setTimeout(() => {
+    Mario.classList.remove('jump');
+  }, 500);
+};
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const MarioPosition = +window
+    .getComputedStyle(Mario)
+    .bottom.replace('px', '');
+
+  if (pipePosition <= 122 && pipePosition > 0 && MarioPosition < 80) {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px `;
+
+    Mario.style.animation = 'none';
+    Mario.style.bottom = `${pipePosition}px `;
+
+    Mario.src = './images/game-over.png';
+    Mario.style.width = '75px';
+    Mario.style.marginLeft = '50px';
+
+    clearInterval(loop);
+  }
+}, 10);
+
+document.addEventListener('keydown', jump);
